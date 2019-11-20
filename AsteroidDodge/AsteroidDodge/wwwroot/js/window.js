086fe4cfb47021068fd14a6eeda23c02180f321e
Melvin <meco0597@gmail.com>
2019-11-20 11:09:30 -0700

    Got bootstrap to finally work

--- AsteroidDodge/AsteroidDodge/wwwroot/js/window.js
+++ AsteroidDodge/AsteroidDodge/wwwroot/js/window.js
@@ -1,26 +1,18 @@
 $(document).ready(function() {
-
-  var windowSize = $(window).width();
-  if (windowSize < 991) {
-    $(".navbar").css("background-color", "black");
-    $(".navbar-collapse").css("background-color", "black");
-    $("#main-text").css("font-size", "57px");
-  } else {
-    $(".navbar").css("background-color", "rgba(0, 0, 0, 0)");
-    $(".navbar-collapse").css("background-color", "rgba(0, 0, 0, 0)");
-    $("#main-text").css("font-size", "75px");
-  }
+    var containerSize = $(".body-projects").width();
+    var gameWindow = $("#game");
+    var newWidth = containerSize * 0.9;
+    gameWindow.attr('width', newWidth);
+    var newHeight = newWidth * (5 / 6);
+    gameWindow.attr('height', newHeight);
+    console.log(containerSize + " " + newWidth + " " + newHeight);
 });
 
-$(window).on("resize", function(e) {
-  var windowSize = $(this).width();
-  if (windowSize < 991) {
-    $(".navbar").css("background-color", "black");
-    $(".navbar-collapse").css("background-color", "black");
-    $("#main-text").css("font-size", "57px");
-  } else {
-    $(".navbar").css("background-color", "rgba(0, 0, 0, 0)");
-    $(".navbar-collapse").css("background-color", "rgba(0, 0, 0, 0)");
-    $("#main-text").css("font-size", "75px");
-  }
+$(window).on("resize", function() {
+    var containerSize = $(".body-projects").width();
+    var gameWindow = $("#game");
+    var newWidth = containerSize * 0.9;
+    gameWindow.attr('width', newWidth);
+    var newHeight = newWidth * (5 / 6);
+    gameWindow.attr('height', newHeight);
 });
