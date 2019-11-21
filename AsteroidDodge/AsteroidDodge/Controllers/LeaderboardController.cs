420b8a806482acb897b38ba05d2959d189366678
Melvin <meco0597@gmail.com>
2019-11-21 09:44:18 -0700

    Added the files for leaderboards and store views

---
+++ AsteroidDodge/AsteroidDodge/Controllers/LeaderboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsteroidDodge.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        // GET: /Leaderboard/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
