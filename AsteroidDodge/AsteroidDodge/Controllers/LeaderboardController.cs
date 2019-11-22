28f4905668b6d45180bceb84795770b00005198d
Melvin <meco0597@gmail.com>
2019-11-22 11:29:45 -0700

    Worked on the leaderboard view

--- AsteroidDodge/AsteroidDodge/Controllers/LeaderboardController.cs
+++ AsteroidDodge/AsteroidDodge/Controllers/LeaderboardController.cs
@@ -11,7 +11,21 @@ namespace AsteroidDodge.Controllers
         // GET: /Leaderboard/
         public IActionResult Index()
         {
-            return View();
+            // This is just random test data to test the view have to replace this with database
+            Dictionary<string, int> testData = new Dictionary<string, int>();
+            int seed = 300;
+            testData.Add("Melvin", seed);
+            testData.Add("Chris", seed - 10);
+            testData.Add("Carl", seed - 15);
+            testData.Add("Judy", seed - 20);
+            testData.Add("Spencer", seed - 25);
+            testData.Add("Sarah", seed - 30);
+            testData.Add("Sean", seed - 35);
+            testData.Add("Jake", seed - 40);
+            testData.Add("Kent", seed - 45);
+            testData.Add("Randy", seed - 50);
+
+            return View(testData);
         }
     }
 }
