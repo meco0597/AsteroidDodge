ed2fe8ee10f2cbf173504e3944543c6964df188c
Zane YARBROUGH <Zane.Yarbrough@biofiredx.com>
2019-11-25 22:06:17 -0700

    removing ignored files????

---
+++ AsteroidDodge/AsteroidDodge/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AsteroidDodge.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AsteroidDodge.Areas.Identity.IdentityHostingStartup))]
+namespace AsteroidDodge.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AsteroidDodgeContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AsteroidDodgeContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AsteroidDodgeContext>();
+            });
+        }
+    }
+}
---
+++ AsteroidDodge/AsteroidDodge/Data/DBInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsteroidDodge.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AsteroidDodge.Data
+{
+    public static class DBInitializer
+    {
+        private static string password = "Password_123";
+        /// <summary>
+        /// Public database initialization method
+        /// </summary>
+        /// <param name="context"></param>
+        public static async Task Initialize(AsteroidDodgeContext context, UserManager<IdentityUser> userManager)
+        {
+            // Check to make sure db is properly created
+            context.Database.EnsureCreated();
+
+            await CreatePlayers(userManager);
+        }
+
+        private static async Task CreatePlayers(UserManager<IdentityUser> userManager)
+        {
+            var players = new IdentityUser[]
+            {
+                new IdentityUser()
+                    {UserName = "Player1@utah.edu", Email = "Player1@utah.edu", EmailConfirmed = true},
+                new IdentityUser()
+                    {UserName = "Player2@utah.edu", Email = "Player2@utah.edu", EmailConfirmed = true},
+                new IdentityUser()
+                    {UserName = "Player3@utah.edu", Email = "Player3@utah.edu", EmailConfirmed = true}
+            };gfh
+
+            foreach (var player in players)
+            {
+                await userManager.CreateAsync(player, password);
+            }
+        }
+    }
+}
