168cb8cf1f401156d1b6e494aebd7e33e448cb29
Samuel <samuelfbridge@gmail.com>
2019-12-09 12:26:58 -0700

    Overview page.

---
+++ AsteroidDodge/AsteroidDodge/Controllers/OverviewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AsteroidDodge.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsteroidDodge.Controllers
+{
+    public class OverviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
